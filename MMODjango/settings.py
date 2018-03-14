91e5743587015416f92edc9d7ab0c40bd78d1064
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-03-14 22:15:34 +0000

    added basic css to the project

--- MMODjango/settings.py
+++ MMODjango/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'dashboard',
+    'pages',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +123,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+
+DJANGO_SIMPLE_ACCOUNTS = {
+    'USER_PROFILE_PYTHON_PATH': 'dashboard.models.UserProfile',
+}
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
