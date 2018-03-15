2410d7742bca118f24276ecc4978f8e04a31d537
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-03-15 22:42:23 +0000

    Commited other files

---
+++ dashboard/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(
+        max_length=12,
+        blank=True,
+    )
+
+    def __unicode__(self):
+        return self.user.username
