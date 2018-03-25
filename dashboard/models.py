87bffdb581b9a107ec610efa075ab4dfeefcbb57
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-03-25 02:16:39 +0100

    handling items improved.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Max
 
 
 # from .models import Monster
@@ -8,13 +9,29 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     experience = models.PositiveIntegerField(default=0)
     gold = models.PositiveIntegerField(default=0)
-    items = models.ManyToManyField('Item')
+    items = models.ManyToManyField('Item', through="UserItem")
+
 
     @property
     def get_level(self):
         experience = Experience.objects.filter(experience__lte=self.experience).last()
         return experience.level
 
+    @property
+    def get_best_helmet(self):
+        return self.items.filter(type=1).latest('arm')
+
+    @property
+    def get_best_armor(self):
+        return self.items.filter(type=2).latest('arm')
+
+    @property
+    def get_best_weapon(self):
+        return self.items.filter(type=4).latest('atk')
+
+    @property
+    def get_best_shield(self):
+        return self.items.filter(type=3).latest('defence')
 
 
 def __unicode__(self):
@@ -36,7 +53,8 @@ class Item(models.Model):
     type = models.CharField(max_length=1, choices=ItemType)
     monsters = models.ManyToManyField('Monster')
     drop_chance = models.PositiveIntegerField(default=1)  #generatePseudoRandomNumber(0, 100) if(rand <= dropChance * 100)
-
+    name = models.CharField(max_length=50, default="")
+    sprite_url = models.CharField(max_length=200, default="")
 
 
 class Monster(models.Model):
@@ -60,3 +78,9 @@ class UserMonster(models.Model):
     @property
     def get_health_percentage(self):
         return (self.health_left/self.monster.health)*100
+
+
+class UserItem(models.Model):
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    equipped = models.BooleanField(default=False)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+import json
 from random import randint
 
 from django.contrib.auth.decorators import login_required
@@ -5,7 +6,7 @@ from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from .models import UserProfile, User, UserMonster, Monster
+from .models import UserProfile, User, UserMonster, Monster, Item, UserItem
 
 
 # Create your views here.
@@ -25,7 +26,34 @@ def adventure(request):
             monster_count = monster_count + 1
     user_monsters = UserMonster.objects.filter(user=user)
 
-    return render(request, 'dashboard/adventure.html', {'user_profile': user_profile, 'monsters': user_monsters})
+    user_items = UserItem.objects.filter(userprofile=user_profile, equipped=True)
+
+    helmet = {}
+    armor = {}
+    weapon = {}
+    shield = {}
+
+    for ui in user_items:
+        if ui.item.type == '1':
+            helmet = ui.item
+        elif ui.item.type == '2':
+            armor = ui.item
+        elif ui.item.type == '3':
+            shield = ui.item
+        else:
+            weapon = ui.item
+
+    context = {
+        'user_profile': user_profile,
+        'user_items': user_items,
+        'monsters': user_monsters,
+        'helmet': helmet,
+        'armor': armor,
+        'shield': shield,
+        'weapon': weapon,
+    }
+
+    return render(request, 'dashboard/adventure.html', context)
 
 
 @login_required
@@ -39,7 +67,7 @@ def ranking(request):
 @login_required()
 def attack(request, user_monster_id):
     level = int(request.GET['level'])
-    damage = randint(0, (9 + level))  # to include damage from weapon
+    damage = randint(0, (9 + level)) + 200  # to include damage from weapon
     print(damage)
     user_monster = UserMonster.objects.get(pk=user_monster_id)
     json_context = {}
@@ -55,21 +83,46 @@ def attack(request, user_monster_id):
             'damage_message': dmg_msg(damage, user_monster.monster.name),
         }
     else:
+        # update user.
         user_monster_to_del = UserMonster.objects.get(pk=user_monster_id)
         user = user_monster_to_del.user
-
         user_profile = UserProfile.objects.get(user=user)
         experience_gained = user_monster_to_del.monster.experience
         user_profile.experience = user_profile.experience + experience_gained
         gold_gained = randint(0, (user_monster_to_del.monster.gold))
         user_profile.gold = user_profile.gold + gold_gained
+
+        # handle items drop.3
+        # killed_monster = Monster.objects.get(pk=)
+        # possible_items = Item.objects.filter(monster=user_monster.monster)
+        # print(possible_items)
+        killed_monster = Monster.objects.get(pk=user_monster.monster_id)
+        loot = killed_monster.items.all()
+        loot_items_str = ""
+
+        looted_items = []
+
+        for item in loot:
+            random_val = randint(0, (10))
+            if random_val + item.drop_chance > 10:
+                user_item = UserItem(item=item, userprofile=user_profile)
+                # user_profile.items.add(item)
+                user_item.save()
+                loot_items_str += item.name
+                loot_items_str += ", "
+                looted_items.append(item.pk)
+
         user_profile.save()
 
         user_monster_to_del.delete()
+
+        # spawn new monster
         monster = Monster.objects.order_by('?').first()
         new_user_monster = UserMonster.objects.create(user=user, monster=monster, health_left=monster.health)
         monster_obj = model_to_dict(monster)
         user_obj = model_to_dict(user_profile)
+        items = Item.objects.filter(pk__in=looted_items).values()
+        # items_obj = model_to_dict(items)
 
         json_context = {
             'killed': True,
@@ -77,33 +130,28 @@ def attack(request, user_monster_id):
             'user_monster_id': new_user_monster.id,
             'monster': monster_obj,
             'user_profile': user_obj,
+            'items': json.dumps(list(items)),
             'level': user_profile.get_level,
-            # 'health_left': user_monster.health_left,
-            # 'percentage': user_monster.get_health_percentage,
-            'loot_message': 'you looted x',
+            'loot_message': loot_msg(loot_items_str, killed_monster.name, gold_gained),
             'damage_message': dmg_msg(damage, user_monster.monster.name),
             'experience_message': exp_msg(experience_gained, user_monster.monster.name)
         }
-        # print('test')
 
     # users = UserProfile.objects.order_by('experience')
     # user_profile = UserProfile.objects.filter(user=user).get()
     # return render(request, 'dashboard/ranking.html', {'users' : users})
     return JsonResponse(json_context)
-    # attack monster
-    # attack monster
-    # attack monster
 
 
 def exp_msg(exp, monster):
-    return str('You gained ' + str(exp) + ' experience points for killing ' + monster +'.')
+    return str('You gained ' + str(exp) + ' experience points for killing ' + monster + '.')
 
 
-def loot_msg(item, monster, gold):
-    if item != None:
-        return str('Loot from' + monster + ': ' + item + '. Gold: ' + gold)
+def loot_msg(items, monster, gold):
+    if items != "":
+        return str('Loot from ' + monster + ': ' + items + '. Gold: ' + str(gold))
     else:
-        return str('Gold: ' + gold + ' from ' + monster + '.')
+        return str('Gold: ' + str(gold) + ' from ' + monster + '.')
 
 
 def dmg_msg(dmg, monster):
